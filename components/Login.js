cfcec88e0cb7620b24df67c41a677deb58fd54d1
al3xf3r <96176190+al3xf3r@users.noreply.github.com>
2021-12-18 08:00:38 +0100

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -4,18 +4,18 @@ import {useMoralis} from "react-moralis";
 function Login() {
     const {authenticate} = useMoralis ();
     return (
-        <div className="bg-black relative ">
+        <div className="bg-black relative text-white ">
             <h1>Login screen</h1>
             <div className="flex flex-col absolute z-50 h-4/6 w-full items-center 
             justify-center space-y-5">
                
                <Image
                className="object-cover rounded-full"
-               src="https://bitcoin4me.it/wp-content/uploads/2021/12/logo12_17_205515.png"
+               src="https://bitcoin4me.it/wp-content/uploads/2021/12/2143902-design-del-logo-della-testa-di-volpe-gratuito-vettoriale.jpg"
                height={200} width={200} />
                
                
-               <button onClick={authenticate} className="bg-white rounded-lg p-5 font-bold animate-pulse">Login to the AlexFer33 1st Server</button>
+               <button onClick={authenticate} className="bg-orange-600 rounded-lg p-5 font-bold animate-pulse">Login to the AlexFer33 1st Server</button>
             </div>
 
             <div className="w-full h-screen">
